de72cfcb6288157e6ed216627536edd78e9faffa
yayal <yayalabayneh2@gmail.com>
2026-06-26 12:35:36 +0300

    removed dependancy

--- inv_purchase_sales/stock_picking_button_groups/__manifest__.py
+++ inv_purchase_sales/stock_picking_button_groups/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Stock Picking Button Groups",
     "summary": "Separate user groups for each stock picking header button",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "category": "Inventory/Inventory",
     "license": "LGPL-3",
     "depends": ["stock"],
